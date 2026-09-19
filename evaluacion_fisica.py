74d987659a05af10760ff0a81a825fb4297970bf
MrNightfall54 <fjnightfall020102@gmail.com>
2026-09-18 21:07:20 -0500

    add evaluacion fisica

---
+++ evaluacion_fisica.py
@@ -0,0 +1,26 @@
+nombre = input("Ingrese su nombre: ")
+edad = int(input("Ingrese su edad: "))
+peso = float(input("Ingrese su peso en kg: "))
+horas_ejercicio = float(input("Ingrese sus horas de ejercicio a la semana: "))
+
+calorias_quemadas = horas_ejercicio * 350
+
+if edad < 18:
+    categoria = "Juvenil"
+else:
+    categoria = "Adulto"
+
+if horas_ejercicio >= 3:
+    estado = "Físicamente Activo"
+else:
+    estado = "Requiere mayor actividad física"
+
+print("\n")
+print("---- Asistente de Evaluación Física -------")
+print(f"Nombre: {nombre}")
+print(f"Edad: {edad} años")
+print(f"Peso: {peso:.2f} kg")
+print(f"Horas de ejercicio semanales: {horas_ejercicio:.2f}")
+print(f"Calorías quemadas estimadas por semana: {calorias_quemadas:.2f}")
+print(f"Categoría: {categoria}")
+print(f"Estado: {estado}")
